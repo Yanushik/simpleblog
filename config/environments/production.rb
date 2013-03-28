7a03ef57b0ec6c3cb3fab80eb5e1a3605a50db6a
Yanushik Cerda <yanushikcerda@gmail.com>
2013-03-28 13:21:20 -0500

    Perhaps working

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -7,7 +7,9 @@ Proj7C::Application.configure do
   # Full error reports are disabled and caching is turned on
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
-
+  #Disable this annoying junk! Temp fix
+  config.action_dispatch.rack_cache[:verbose] = false
+  
   # Disable Rails's static asset server (Apache or nginx will already do this)
   config.serve_static_assets = false
 
@@ -15,7 +17,7 @@ Proj7C::Application.configure do
   config.assets.compress = true
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = false
+  config.assets.compile = true
 
   # Generate digests for assets URLs
   config.assets.digest = true
